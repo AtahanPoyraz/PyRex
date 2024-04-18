5010f0d689e6d95d2e1493f41c68feb3fc9ecb56
Atahan Poyraz <atahanpoyraz@gmail.com>
2024-04-18 11:20:48 +0300

    Update main.py
    port option added

--- PyRex/main.py
+++ PyRex/main.py
@@ -22,21 +22,23 @@ class Main:
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("-m", "--module", required=True, choices=['ftp', 'ssh', 'telnet'], help="Select a module: ftp, ssh, or telnet.")
         self.parser.add_argument("-t", "--target", required=True, help="Enter the IP or Domain.")
+        self.parser.add_argument("-p", "--port", required=False, type=int, help="Enter Target Port.")
         self.parser.add_argument("-u", "--user", required=True, help="Username for login attempt.")
-        self.parser.add_argument("-to", "--timeout", required=True, type=int, help="Timeout per connection attempt for ssh and telnet module.")
+        self.parser.add_argument("-to", "--timeout", required=True, type=int, help="Timeout per connection attempt for telnet module.")
         self.parser.add_argument("-w", "--wordlist", required=True, help="Location of the file with passwords.")
 
         args = self.parser.parse_args()
 
         self.module = args.module
         self.target = args.target
+        self.port = args.port
         self.user = args.user
         self.wordlist = args.wordlist
         self.timeout = args.timeout
 
-        self.run(module=self.module, target=self.target, username=self.user, wordlist=self.wordlist, timeout=self.timeout)
+        self.run(module=self.module, target=self.target, port=self.port ,username=self.user, wordlist=self.wordlist, timeout=self.timeout)
 
-    def run(self, *c, module : str, target : str, username : str, wordlist : str, timeout : int = 3) -> None:
+    def run(self, *c, module : str, target : str, port : int, username : str, wordlist : str, timeout : int = 3) -> None:
         match module.lower():
             case "ftp":
                 if target != "" and username != "" and wordlist != "":
@@ -47,7 +49,7 @@ class Main:
 
             case "ssh":
                 if target != "" and username != "" and wordlist != "":
-                    SSH_BRUTEFORCE(target=target, username=username, timeout=timeout, wordlist=wordlist).start()
+                    SSH_BRUTEFORCE(target=target, port=port, username=username, timeout=timeout, wordlist=wordlist).start()
 
                 else:
                     print("Missing Input Value!")
